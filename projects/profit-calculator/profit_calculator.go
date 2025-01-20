82b440b42931f0ac9aadc60d9a1f09efa6106beb
Akshat Shah <ashah85@binghamton.edu>
2025-01-20 17:46:42 -0500

    Refactor profit calculator to use functions for user input and profit calculations

--- projects/profit-calculator/profit_calculator.go
+++ projects/profit-calculator/profit_calculator.go
@@ -5,25 +5,31 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to Profit Calculator!")
 
-	var revenue float64
-	var expenses float64
-	taxRate := 2.5
+	revenue := getUserInput("Please enter revenue: ")
 
-	fmt.Print("Please enter revenue: ")
-	fmt.Scan(&revenue)
+	expenses := getUserInput("Please enter expenses: ")
 
-	fmt.Print("Please enter expenses: ")
-	fmt.Scan(&expenses)
+	taxRate := getUserInput("Please enter Tax rate: ")
 
-	fmt.Print("Please enter Tax rate: ")
-	fmt.Scan(&taxRate)
-
-	earningsBeforeTax := revenue - expenses
-	earningsAfterTax := earningsBeforeTax * (1 - (taxRate / 100))
-
-	ratio := earningsBeforeTax / earningsAfterTax
+	earningsBeforeTax, earningsAfterTax, ratio := calculateProfit(revenue, expenses, taxRate)
 
 	fmt.Println("Earnings before tax: ", earningsBeforeTax)
 	fmt.Println("Earnings After tax(Profit): ", earningsAfterTax)
 	fmt.Println("Ratio: ", ratio)
+}
+
+func calculateProfit(revenue, expenses, taxRate float64) (earningsBeforeTax float64, earningsAfterTax float64, ratio float64) {
+	earningsBeforeTax = revenue - expenses
+	earningsAfterTax = earningsBeforeTax * (1 - (taxRate / 100))
+
+	ratio = earningsBeforeTax / earningsAfterTax
+
+	return earningsBeforeTax, earningsAfterTax, ratio
+}
+
+func getUserInput(text string) float64 {
+	var userInput float64
+	fmt.Print(text)
+	fmt.Scan(&userInput)
+	return userInput
 }
