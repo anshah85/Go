9cfe204e7cdf7895300d3892afe276297dabd770
Akshat Shah <ashah85@binghamton.edu>
2025-01-20 01:13:11 -0500

    Enhance investment calculator to accept user input and calculate future real value considering inflation

--- practice/Go-essentials/investment_calculator.go
+++ practice/Go-essentials/investment_calculator.go
@@ -6,12 +6,26 @@ import (
 )
 
 func main() {
-	fmt.Print("Hello, World! Here, I am learning GoLang from Scratch.")
+	fmt.Print("Hello, World! Here, I am learning GoLang from Scratch.\n")
 
-	var investmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
-	var years float64 = 10
+	const inflationRate = 3.5
 
-	var fultureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	fmt.Print("\nFuture value of the investment is: ", fultureValue)
+	var investmentAmount float64
+	var expectedReturnRate float64
+	var years float64
+
+	fmt.Print("Investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	fultureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := fultureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println("Future value of the investment is: ", fultureValue)
+	fmt.Println("Future real value of the investment is: ", futureRealValue)
 }
